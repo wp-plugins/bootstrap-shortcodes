64919c097c5226d24470ddd1f1ea6d9e905a127d
sinetheta <sinetheta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-20 22:59:10 +0000

    tagging version 3.0
    git-svn-id: https://plugins.svn.wordpress.org/bootstrap-shortcodes/tags/3.0@898972 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/bs_labels.php
+++ inc/bs_labels.php
@@ -1,13 +1,11 @@
-<?php 
+<?php
+function bs_labels( $params, $content=null ) {
+    extract( shortcode_atts( array(
+        'type' => 'default'
+    ), $params ) );
 
-function bs_labels($params, $content = null){
-    extract(shortcode_atts(array(
-        'type' => 'default',
-        'text' => 'my label'
-    ), $params));
-
-    $content = preg_replace('/<br class="nc".\/>/', '', $content);
-    $result = '<span class="label label-'.$type.'">'.$text.'</span>';
+    $content = preg_replace( '/<br class="nc".\/>/', '', $content );
+    $result = '<span class="label label-' . $type . '">' . $content . '</span>';
     return force_balance_tags( $result );
 }
-add_shortcode('bs_label', 'bs_labels');
+add_shortcode( 'bs_label', 'bs_labels' );
